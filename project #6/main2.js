c8d6a61313dec65a22682bf48718718afc65ae5b
Shimaa-esmat~ <shymaaesam01@gmail.com>
2024-05-03 09:00:37 +0300

    still handel input error

---
+++ project #6/main2.js
@@ -0,0 +1,26 @@
+
+// get localStorage value
+let cardNum = window.localStorage.getItem("cardNum");
+let user = window.localStorage.getItem("userName");
+let MM = window.localStorage.getItem("month");
+let YY = window.localStorage.getItem("year");
+let cvc = window.localStorage.getItem("cvc");
+
+
+if (+MM < 10) {
+    MM = "0" + MM;
+}
+
+// get innerText of data
+let cardValue = document.querySelector('.num-value');
+let cardName = document.querySelector('#name-value');
+let date = document.querySelector('#date-value');
+let cvcValue = document.querySelector('#cvc-value');
+
+
+cardValue.textContent = cardNum;
+cardName.innerHTML = user;
+cvcValue.innerHTML = cvc;
+date.innerHTML = `${MM}/${YY}`;
+
+
