120aba883b2c92154c0681ab374898e039173877
Shimaa-esmat~ <shymaaesam01@gmail.com>
2024-05-03 08:15:23 +0300

    completed

---
+++ project #6/main.js
@@ -0,0 +1,37 @@
+let UserInfo = document.querySelectorAll('input');
+console.log(UserInfo);
+
+UserInfo.forEach(e =>{
+    console.log(e.id);
+    e.oninput = function (){
+        if(e.id === "card-name" ){
+            window.sessionStorage.setItem("userName",e.value);
+        }
+        if(e.id === "MM" ){
+            window.sessionStorage.setItem("month",e.value);
+        }
+        if(e.id === "card-num" ){
+            window.sessionStorage.setItem("cardNum",e.value);
+        }
+        if(e.id === "YY" ){
+            window.sessionStorage.setItem("year",e.value);
+        }
+        if(e.id === "cvc" ){
+            window.sessionStorage.setItem("cvc",e.value);
+        }
+
+    }
+
+});
+
+
+let btn = UserInfo.item(5);
+
+btn.addEventListener("click",function(){
+    window.localStorage.setItem("userName",window.sessionStorage.getItem("userName"));
+    window.localStorage.setItem("month",window.sessionStorage.getItem("month"));
+    window.localStorage.setItem("cardNum",window.sessionStorage.getItem("cardNum"));
+    window.localStorage.setItem("year",window.sessionStorage.getItem("year"));
+    window.localStorage.setItem("cvc",window.sessionStorage.getItem("cvc"));
+})
+
