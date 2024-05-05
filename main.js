778054c6e4a31bf396cb459522b4f492883860d5
Shimaa-esmat~ <shymaaesam01@gmail.com>
2024-05-05 15:28:29 +0300

    finished

--- main.js
+++ main.js
@@ -1,36 +1,196 @@
-let UserInfo = document.querySelectorAll('input');
+// variable
+let userInfo = document.querySelectorAll('input');
+let form = document.querySelector("form")
+let nameInput = userInfo.item(0);
+let cardNumberInput = userInfo.item(1);
+let monthNumberInput = userInfo.item(2);
+let yearNumberInput = userInfo.item(3);
+let cvcNumberInput = userInfo.item(4);
+let cardValue = document.querySelector('.num-value');
+let cardName = document.querySelector('#name-value');
+let date = document.querySelector('#date-value');
+let cvcValue = document.querySelector('#cvc-value');
 
-UserInfo.forEach(e =>{
+
+ let userName = "";
+ let cardNumber = "";
+ let month = "0";
+ let year = "00";
+ let cvc = "";
+
+
+// let cardNumber = window.localStorage.getItem("cardNum");
+// let userName = window.localStorage.getItem("userName");
+// let month = window.localStorage.getItem("month");
+// let year = window.localStorage.getItem("year");
+// let cvc = window.localStorage.getItem("cvc");
+
+
+
+
+    
+    // get input value
+userInfo.forEach(e =>{
     e.oninput = function (){
         if(e.id === "card-name" ){
-            window.sessionStorage.setItem("userName",e.value);
+            userName = e.value
+            window.sessionStorage.setItem("userName",userName)
+            cardName.innerHTML = userName;
         }
         if(e.id === "MM" ){
-            window.sessionStorage.setItem("month",e.value);
+            month = e.value
         }
         if(e.id === "card-num" ){
-            window.sessionStorage.setItem("cardNum",e.value);
+            cardNumber = e.value
+            cardValue.textContent = cardNumber;
         }
         if(e.id === "YY" ){
-            window.sessionStorage.setItem("year",e.value);
+            year=e.value
         }
+        
         if(e.id === "cvc" ){
-            window.sessionStorage.setItem("cvc",e.value);
-        }
-        if(e.id === "sub-btn" ){
-            window.localStorage.setItem("btn",e);
-        }
+            cvc = e.value;
+            cvcValue.innerHTML = cvc;
 
+        }        
+    }
+});
+
+
+
+
+
+
+// export{userName , cardNumber, cvc, month, year};/
+
+
+
+
+// functions to test validation
+function error (input,msg){
+    let parent = input.parentElement;
+    let error = parent.querySelector('small');
+    input.style.border = "hsl(0, 100%, 66%)  solid  1px"
+    error.textContent = msg;
+}
+
+function success (input){
+    let parent = input.parentElement;
+    let error = parent.querySelector('small');
+    input.style.border = "hsl(270, 3%, 87%) solid  2px"
+    error.textContent = "";
+
+}
+
+function validateStringField(inputField,inputFieldData){
+    if  (inputFieldData == ""){
+        error(inputField,'Username cannot be blank.');  
+        return false;  
+    }
+    success(inputField);
+    return true;
+}
+
+
+function validateNumberField(inputField,inputFieldData,digit,min,max){
+    if  (!inputFieldData){
+        error(inputField,'Number cannot be blank.');
+        return false 
+    }
+    if (!parseFloat(inputFieldData)) {
+        error(inputField,'Wrong format, number only');
+        return false;
+    }
+    if((digit !== null) && (+inputFieldData <= Math.pow(10,digit-1) || +inputFieldData >= Math.pow(10,digit))){
+        error(inputField,`Number must be ${digit} digit.`);
+        return false;
+    }
+    if((max !== null) && (+inputFieldData < min || +inputFieldData > max )){
+        error(inputField,`Number must be larger than ${min} and smaller than ${max}.`);
+        console.log("error")
+        return false;
+    }
+    
+    success(inputField);
+    return true;
+}
+
+
+
+
+
+
+
+userInfo.forEach(e =>{
+        
+  
+    e.oninput = function (){
+        if(e.id === "card-name" ){
+            userName = e.value
+            window.sessionStorage.setItem("userName",userName)
+            cardName.innerHTML = userName;
+            validateStringField(nameInput,userName);
+        }
+        if(e.id === "MM" ){
+            month = e.value
+            window.sessionStorage.setItem("month",month)
+            validateNumberField(monthNumberInput,month,null,1,12);            
+
+        }
+        if(e.id === "card-num" ){
+            cardNumber = e.value
+            window.sessionStorage.setItem("cardNumber",cardNumber)
+            cardValue.textContent = cardNumber;
+            validateNumberField(cardNumberInput,cardNumber,16,null,null)
+        }
+        if(e.id === "YY" ){
+            year=e.value;
+            window.sessionStorage.setItem("year",year)
+            validateNumberField(yearNumberInput,year,null,24,99);
+        }
+        
+        if(e.id === "cvc" ){
+            cvc = e.value;
+            window.sessionStorage.setItem("cvc",cvc)
+            cvcValue.innerHTML = cvc;
+            validateNumberField(cvcNumberInput,cvc,null,100,1000);
+
+        }       
     }
 
-});
-let btn = UserInfo.item(5)
 
-btn.addEventListener("click",function(){
-    window.localStorage.setItem("userName",window.sessionStorage.getItem("userName"));
-    window.localStorage.setItem("month",window.sessionStorage.getItem("month"));
-    window.localStorage.setItem("cardNum",window.sessionStorage.getItem("cardNum"));
-    window.localStorage.setItem("year",window.sessionStorage.getItem("year"));
-    window.localStorage.setItem("cvc",window.sessionStorage.getItem("cvc"));
-})
+
+    // let isNameValid = validateStringField(nameInput,userName);
+    // let iscardNunmberValid = validateNumberField(cardNumberInput,cardNumber,16,null,null);
+    // let isCvcNumberValid = validateNumberField(cvcNumberInput,cvc,null,100,1000);
+    // let isMonthNumberValid = validateNumberField(monthNumberInput,month,null,1,12);
+    // let isYearNumberValid = validateNumberField(yearNumberInput,year,null,24,99);
+});
+if (+month < 10) {
+    month = "0" + month;
+}
+date.innerHTML = `${month}/${year}`;
+
+// submitting form
+
+form.addEventListener("submit",function(e){
+    console.log(userName);
+
+
+    
+    let isNameValid = validateStringField(nameInput,userName);
+    let iscardNunmberValid = validateNumberField(cardNumberInput,cardNumber,16,null,null);
+    let isCvcNumberValid = validateNumberField(cvcNumberInput,cvc,null,100,1000);
+    let isMonthNumberValid = validateNumberField(monthNumberInput,month,null,1,12);
+    let isYearNumberValid = validateNumberField(yearNumberInput,year,null,24,99);
+    
+    if(!(isNameValid&&iscardNunmberValid&&isCvcNumberValid&&isMonthNumberValid&&isYearNumberValid)){
+        e.preventDefault();
+    }else{
+        return
+    }
+});
+
+
+
 
--- main2.js
+++ main2.js
@@ -1,14 +1,17 @@
+// import { userName, cardNumber, cvc, month, year } from './main.js';
 
+// console.log(userName);
 // get localStorage value
-let cardNum = window.localStorage.getItem("cardNum");
-let user = window.localStorage.getItem("userName");
-let MM = window.localStorage.getItem("month");
-let YY = window.localStorage.getItem("year");
-let cvc = window.localStorage.getItem("cvc");
+let cardNumber = window.sessionStorage.getItem("cardNum");
+let userName = window.sessionStorage.getItem("userName");
+let month = window.sessionStorage.getItem("month");
+let year = window.sessionStorage.getItem("year");
+let cvc = window.sessionStorage.getItem("cvc");
 
 
-if (+MM < 10) {
-    MM = "0" + MM;
+
+if (+month < 10) {
+    month = "0" + month;
 }
 // get innerText of data
 let cardValue = document.querySelector('.num-value');
@@ -16,9 +19,9 @@ let cardName = document.querySelector('#name-value');
 let date = document.querySelector('#date-value');
 let cvcValue = document.querySelector('#cvc-value');
 
-cardValue.textContent = cardNum;
-cardName.innerHTML = user;
+cardNumber.textContent = cardNumber;
+cardName.innerHTML = userName;
 cvcValue.innerHTML = cvc;
-date.innerHTML = `${MM}/${YY}`;
+date.innerHTML = `${month}/${year}`;
 
 
