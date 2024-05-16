67077066eba67a1c586957573717a854db63d55e
Shimaa-esmat~ <shymaaesam01@gmail.com>
2024-05-16 16:49:11 +0300

    control move by keycode

--- project#7/main.js
+++ project#7/main.js
@@ -3,27 +3,16 @@ let pageLinks = document.querySelectorAll('.links');
 // console.log(pageLinks.innerHTML);
 let claseBtn = document.querySelector('.close')
 let sideBar = document.querySelector(".lines");
-
 // let bageLinks - 
-
 claseBtn.addEventListener('click',function(){
     innerSideBar.style.display = "none";
     sideBar.style.display = "block";
-
 })
 sideBar.addEventListener('click',function(){
     sideBar.style.display = "none";
     innerSideBar.style.display = "flex";
-
 })
-
-
-
-
-
- console.log(pageLinks[0].textContent.toLowerCase().includes('home'));
-
-
+console.log(pageLinks[0].textContent.toLowerCase().includes('home'));
 // fetch json file
 let destinations = new Array();
 
@@ -46,19 +35,18 @@ let moonImage = document.querySelector('.moon-photo > img');
 let moonDescription = document.querySelector('.bio');
 let distanceValue = document.querySelector('.dis-value');
 let travelTime = document.querySelector('.time-value');
-
-window.onload = function() {
-    moonImage.style.animation = "newmoon 5s forwards";
-};
+// window.onload = function() {
+//     moonImage.style.animation = "newmoon 5s forwards";
+// };
 getData("destinations").then(() => {
     value.forEach(e => {
         moons.forEach(el =>{
             el.addEventListener('click',function(){
                 moons.forEach(ele =>{
                 ele.classList.remove('active-moon');
-                
                 })
                 el.classList.add('active-moon')
+                // console.log(el.nextElementSibling);
                 if(el.textContent.toLowerCase() == e.name.toLowerCase()){
                     moonName.innerHTML = e.name.toUpperCase();
                     moonImage.setAttribute('src',e.images.png);
@@ -66,14 +54,23 @@ getData("destinations").then(() => {
                     distanceValue.innerHTML = e.distance
                     travelTime.innerHTML = e.travel
                 }
-                // moonImage.style.width = "0px";
-                // moonImage.style.height = "0px";
-                // moonImage.style.animation = "newmoon 0.5s forwards";
+            })
         })
-    })
-})    
+    })    
 
 });
+
+document.onkeydown = checkKey;
+function checkKey(pressedKay) {
+        pressedKay = pressedKay || window.event;
+        console.log(pressedKay.keyCode)
+
+        }
+
+
+
+
+
 
 
 // crew data
@@ -97,15 +94,12 @@ getData("crew").then(() => {
                 })
                 el.classList.add('active-crew')
                 if(el.id.toLowerCase() == e.name.toLowerCase()){
-                    console.log("fff");
+                    // console.log("fff");
                     crewName.innerHTML = e.name.toUpperCase();
                     crewImage.setAttribute('src',e.images.png);
                     crewJob.innerHTML = e.role
                     bio.innerHTML = e.bio
-
                 }
-            
-            
         })
     })
 })    
@@ -115,8 +109,8 @@ getData("crew").then(() => {
 
 
 
-// tech data
 
+// tech data
 let technology = document.querySelectorAll(".tech");
 let tschnologyName = document.querySelector('.tech-name')
 let technologyImageLandscape = document.querySelector('.landscape');
@@ -136,17 +130,215 @@ getData("technology").then(() => {
                 })
                 el.classList.add('active-tech')
                 if(el.id.toLowerCase() == e.name.toLowerCase()){
-                    console.log("fff");
                     tschnologyName.innerHTML = e.name.toUpperCase();
                     technologyImagePortrait.setAttribute('src',e.images.portrait);
                     technologyImageLandscape.setAttribute('src',e.images.landscape);
                     description.innerHTML = e.description
-
                 }
-            
-            
         })
     })
 })    
 
 });
+
+
+
+document.onkeydown = checkKey;
+function checkKey(pressedKay) {
+        pressedKay = pressedKay || window.event;
+            if (pressedKay.keyCode == '39') {
+                for(let i= 0;i<moons.length-1;i++){
+                    if(moons[i].classList.contains('active-moon')){
+
+                        moons.forEach(ele =>{
+                        ele.classList.remove('active-moon');
+                        })
+                        
+                        moons[i].nextElementSibling.classList.add('active-moon')
+                        getData("destinations").then(() => {
+                            value.forEach(e => {
+                        if(moons[i].nextElementSibling.textContent.toLowerCase() == e.name.toLowerCase()){
+                            moonName.innerHTML = e.name.toUpperCase();
+                            moonImage.setAttribute('src',e.images.png);
+                            moonDescription.innerHTML = e.description
+                            distanceValue.innerHTML = e.distance
+                            travelTime.innerHTML = e.travel
+                            }
+                        })    
+
+                    });
+                        break;
+                        }
+                    }
+                for(let i= 0;i<crews.length-1;i++){
+                    if(crews[i].classList.contains('active-crew')){
+                        crews.forEach(ele =>{
+                        ele.classList.remove('active-crew');
+                        })
+                        console.log(crews[i].nextElementSibling)
+                        crews[i].nextElementSibling.classList.add('active-crew')
+                        getData("crew").then(() => {
+                            value.forEach(e => {
+                        if(crews[i].nextElementSibling.id.toLowerCase() == e.name.toLowerCase()){
+                            crewName.innerHTML = e.name.toUpperCase();
+                            crewImage.setAttribute('src',e.images.png);
+                            crewJob.innerHTML = e.role
+                            bio.innerHTML = e.bio
+                            }
+                        })    
+
+                    });
+                        break;
+                        }
+                        }
+                        if(window.innerWidth < 1050){
+                        for(let i= 0;i<technology.length-1;i++){
+                            if(technology[i].classList.contains('active-tech')){
+                                technology.forEach(ele =>{
+                                ele.classList.remove('active-tech');
+                                })
+                                console.log(technology[i].nextElementSibling)
+                                technology[i].nextElementSibling.classList.add('active-tech')
+                                getData("technology").then(() => {
+                                    value.forEach(e => {
+                                if(technology[i].nextElementSibling.id.toLowerCase() == e.name.toLowerCase()){
+                                    tschnologyName.innerHTML = e.name.toUpperCase();
+                                    technologyImagePortrait.setAttribute('src',e.images.portrait);
+                                    technologyImageLandscape.setAttribute('src',e.images.landscape);
+                                    description.innerHTML = e.description
+                                    }
+                                })    
+        
+                            });
+                                break;
+                                }
+                                }
+                        }
+                    }
+                    else if (pressedKay.keyCode == '37') {
+                        for(let i= moons.length-1;i >0;i--){
+                            if(moons[i].classList.contains('active-moon')){
+                                moons.forEach(ele =>{
+                                ele.classList.remove('active-moon');
+                                })
+                                
+                                moons[i].previousElementSibling.classList.add('active-moon')
+                                getData("destinations").then(() => {
+                                    value.forEach(e => {
+                                    if(moons[i].previousElementSibling.textContent.toLowerCase() == e.name.toLowerCase()){
+                                    moonName.innerHTML = e.name.toUpperCase();
+                                    moonImage.setAttribute('src',e.images.png);
+                                    moonDescription.innerHTML = e.description
+                                    distanceValue.innerHTML = e.distance
+                                    travelTime.innerHTML = e.travel
+                                    }
+
+                                })    
+    
+                            });
+                                break;
+                            }
+                            }
+
+                        for(let i= crews.length-1;i >0;i--){
+                            if(crews[i].classList.contains('active-crew')){
+                                crews.forEach(ele =>{
+                                ele.classList.remove('active-crew');
+                                })
+                                console.log(crews[i].previousElementSibling)
+                                crews[i].previousElementSibling.classList.add('active-crew')
+                                getData("crew").then(() => {
+                                    value.forEach(e => {
+                                        console.log(crews[i].previousElementSibling.textContent.toLowerCase())
+                                    if(crews[i].previousElementSibling.id.toLowerCase() == e.name.toLowerCase()){
+                                        crewName.innerHTML = e.name.toUpperCase();
+                                        crewImage.setAttribute('src',e.images.png);
+                                        crewJob.innerHTML = e.role
+                                        bio.innerHTML = e.bio
+                                    }
+                                })        
+                            });
+                                break;
+                            }
+                            }
+                            if(window.innerWidth < 1050){
+                                for(let i= technology.length-1;i > 0;i--){
+                                    if(technology[i].classList.contains('active-tech')){
+                                        technology.forEach(ele =>{
+                                        ele.classList.remove('active-tech');
+                                        })
+                                        console.log(technology[i].previousElementSibling)
+                                        technology[i].previousElementSibling.classList.add('active-tech')
+                                        getData("technology").then(() => {
+                                            value.forEach(e => {
+                                        if(technology[i].previousElementSibling.id.toLowerCase() == e.name.toLowerCase()){
+                                            tschnologyName.innerHTML = e.name.toUpperCase();
+                                            technologyImagePortrait.setAttribute('src',e.images.portrait);
+                                            technologyImageLandscape.setAttribute('src',e.images.landscape);
+                                            description.innerHTML = e.description
+                                            }
+                                        })    
+                
+                                    });
+                                        break;
+                                        }
+                                        }
+                                }
+                        }
+                        else if (pressedKay.keyCode == '38') {
+                            if(window.innerWidth > 1050){
+                                for(let i= technology.length-1;i > 0;i--){
+                                    if(technology[i].classList.contains('active-tech')){
+                                        technology.forEach(ele =>{
+                                        ele.classList.remove('active-tech');
+                                        })
+                                        console.log(technology[i].previousElementSibling)
+                                        technology[i].previousElementSibling.classList.add('active-tech')
+                                        getData("technology").then(() => {
+                                            value.forEach(e => {
+                                        if(technology[i].previousElementSibling.id.toLowerCase() == e.name.toLowerCase()){
+                                            tschnologyName.innerHTML = e.name.toUpperCase();
+                                            technologyImagePortrait.setAttribute('src',e.images.portrait);
+                                            technologyImageLandscape.setAttribute('src',e.images.landscape);
+                                            description.innerHTML = e.description
+                                            }
+                                        })    
+                
+                                    });
+                                        break;
+                                        }
+                                        }
+
+
+
+                                }
+                        }
+                        else if (pressedKay.keyCode == '40') {
+                            if(window.innerWidth > 1050){
+                                for(let i= 0;i<technology.length-1;i++){
+                                    if(technology[i].classList.contains('active-tech')){
+                                        technology.forEach(ele =>{
+                                        ele.classList.remove('active-tech');
+                                        })
+                                        console.log(technology[i].nextElementSibling)
+                                        technology[i].nextElementSibling.classList.add('active-tech')
+                                        getData("technology").then(() => {
+                                            value.forEach(e => {
+                                        if(technology[i].nextElementSibling.id.toLowerCase() == e.name.toLowerCase()){
+                                            tschnologyName.innerHTML = e.name.toUpperCase();
+                                            technologyImagePortrait.setAttribute('src',e.images.portrait);
+                                            technologyImageLandscape.setAttribute('src',e.images.landscape);
+                                            description.innerHTML = e.description
+                                            }
+                                        })    
+                
+                                    });
+                                        break;
+                                        }
+                                        }
+
+                                }
+                        }
+}
+
+
